a411573539ca24dc855c4b84e3d4241cb6a31aa6
John Roman <john.roman@arteric.com>
2016-12-14 00:37:16 -0500

    jrr- adding some methods to get some logging in place

--- src/js/form.js
+++ src/js/form.js
@@ -1,14 +1,41 @@
-var form = {
+const form = {
   // Initial Values
-  siteName: document.querySelector('#siteName').value,
-  env: document.querySelector('#env').value,
-  docRoot: document.querySelector('#docRoot').value,
-  dbName: document.querySelector('#dbName').value,
-  repoName: document.querySelector('#repoName').value,
-  createDB: document.querySelector('#dbChk').checked,
-  getRepo: document.querySelector('#repoChk').checked,
+  siteName: document.querySelector('#siteName'),
+  env: document.querySelector('#env'),
+  docRoot: document.querySelector('#docRoot'),
+  dbName: document.querySelector('#dbName'),
+  repoName: document.querySelector('#repoName'),
+  createDB: document.querySelector('#dbChk'),
+  getRepo: document.querySelector('#repoChk'),
 
-  createDatabase: () => {
-    
-  }
-}
+  cloneRepo: function() {
+    if(!this.getRepo.checked) {
+      console.log('clone repo? ', this.getRepo.checked);
+    }
+    console.log('clone repo? ', this.getRepo.checked);
+  },
+
+  createDatabase: function() {
+    if(!this.createDB.checked) {
+      console.log('create db? ', this.createDB.checked);
+    }  // i love you :);
+    console.log('create db? ', this.createDB.checked);
+  },
+
+  sendForm: function() {
+    runArr = [
+      this.siteName.value,
+      this.env.value,
+      this.docRoot.value,
+      this.dbName.value,
+      this.repoName.value,
+      this.createDB.checked,
+      this.getRepo.checked,
+    ];
+
+    runArr.map(x => console.log(x));
+  },
+};
+
+// const submitBtn = document.querySelector('.submit-btn');
+// form.submitForm(submitBtn)
