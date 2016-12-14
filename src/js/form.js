6e909cbfbf72cc475975f2ce8031bb63dc647ca3
John Roman <john.roman@arteric.com>
2016-12-13 22:14:16 -0500

    jrr- creating initial variables, base css, html base elements

---
+++ src/js/form.js
@@ -0,0 +1,14 @@
+var form = {
+  // Initial Values
+  siteName: document.querySelector('#siteName').value,
+  env: document.querySelector('#env').value,
+  docRoot: document.querySelector('#docRoot').value,
+  dbName: document.querySelector('#dbName').value,
+  repoName: document.querySelector('#repoName').value,
+  createDB: document.querySelector('#dbChk').checked,
+  getRepo: document.querySelector('#repoChk').checked,
+
+  createDatabase: () => {
+    
+  }
+}
